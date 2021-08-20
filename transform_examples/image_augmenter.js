a0c3fa36318de9f256d7bf096a75d638fec00add
josephrocca <1167575+josephrocca@users.noreply.github.com>
2021-08-20 20:16:36 +0800

    aaa

--- transform_examples/image_augmenter.js
+++ transform_examples/image_augmenter.js
@@ -1,4 +1,4 @@
-let size = 224; // image width/height
+let size = 224; // image width and height 
 let canvas = new OffscreenCanvas(size, size);
 let ctx = canvas.getContext("2d");
 ctx.save();
@@ -18,6 +18,8 @@ async function transform(data) {
     ctx.save(); // we need to save immediately after restoring because restoring pops the save off the stack, so we need a "save" for the next iteration of the loop: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/restore
     ctx.clearRect(0, 0, size, size);
 
+    // if(r() < 0.5) randomBackground();
+
     let transforms = [rotate, shift, scale, filter];
     shuffleArray(transforms);
     transforms.forEach(t => t());
@@ -33,12 +35,23 @@ async function transform(data) {
   return outputs;
 }
 
+// function randomBackground() {
+//   ctx.fillStyle = randomColor();
+//   ctx.fillRect(0, 0, size, size);
+//   if(r() < 0.5) {
+//     let gradient = ctx.createLinearGradient(r(-size, size*2), r(-size, size*2), r(-size, size*2), r(-size, size*2));
+//     gradient.addColorStop(0, randomColor());
+//     gradient.addColorStop(1, randomColor());
+//     ctx.fillStyle = gradient;
+//     ctx.fillRect(0, 0, size, size);
+//   }
+// }
 
 function rotate() {
   let maxRotateDeg = 15;
-  if(Math.random() < 0.5) {
+  if(r() < 0.5) {
     ctx.translate(size/2, size/2);
-    ctx.rotate((-maxRotateDeg + Math.random()*maxRotateDeg*2) * (2*Math.PI/360));
+    ctx.rotate((-maxRotateDeg + r()*maxRotateDeg*2) * (2*Math.PI/360));
     ctx.translate(-size/2, -size/2);
   }
 }
@@ -46,28 +59,28 @@ function rotate() {
 
 function shift() {
   let maxShift = 0.2; // <-- fraction of size
-  if(Math.random() < 0.5) {
-    ctx.translate(-size*maxShift + Math.random()*size*maxShift*2, -size*maxShift + Math.random()*size*maxShift*2);
+  if(r() < 0.5) {
+    ctx.translate(-size*maxShift + r()*size*maxShift*2, -size*maxShift + r()*size*maxShift*2);
   }    
 }
 
 
 function scale() {
   let maxScale = 0.7; // <-- fraction of size
-  ctx.scale(Math.random() < 0.5 ? (1-maxScale)+Math.random()*maxScale*2 : 1, Math.random() < 0.5 ? (1-maxScale)+Math.random()*maxScale*2 : 1);
+  ctx.scale(r() < 0.5 ? (1-maxScale)+r()*maxScale*2 : 1, r() < 0.5 ? (1-maxScale)+r()*maxScale*2 : 1);
 }
 
 
 function filter() {
-  if(Math.random() < 0.7) {
+  if(r() < 0.7) {
     let filterStr = "";
     let filters = [
-      () => { if(Math.random() < 0.5) filterStr += ` hue-rotate(${-10+Math.floor(Math.random()*20)}deg)`; },
-      () => { if(Math.random() < 0.5) filterStr += ` blur(${Math.floor(Math.random()*7)}px)`; },
-      () => { if(Math.random() < 0.5) filterStr += ` grayscale(${Math.random()*0.4})`; },
-      () => { if(Math.random() < 0.5) filterStr += ` brightness(${0.5+Math.random()*1.5})`; },
-      () => { if(Math.random() < 0.5) filterStr += ` contrast(${0.5+Math.random()*5})`; },
-      () => { if(Math.random() < 0.5) filterStr += ` saturate(${0.5+Math.random()*5})`; },
+      () => { if(r() < 0.5) filterStr += ` hue-rotate(${-10+Math.floor(r()*20)}deg)`; },
+      () => { if(r() < 0.5) filterStr += ` blur(${Math.floor(r()*7)}px)`; },
+      () => { if(r() < 0.5) filterStr += ` grayscale(${r()*0.4})`; },
+      () => { if(r() < 0.5) filterStr += ` brightness(${0.5+r()*1.5})`; },
+      () => { if(r() < 0.5) filterStr += ` contrast(${0.5+r()*5})`; },
+      () => { if(r() < 0.5) filterStr += ` saturate(${0.5+r()*5})`; },
     ];
     shuffleArray(filters);
     filters.forEach(f => f());
@@ -80,5 +93,20 @@ function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function randomColor() {
+  return `rgb(${Math.floor(r()*255)}, ${Math.floor(r()*255)}, ${Math.floor(r()*255)})`;
+}
+
+function r(a, b) {
+  if(a === undefined && b === undefined) {
+    return Math.random();
+  } else if(b === undefined) {
+    return Math.random()*a;
+  } else {
+    if(b < a) throw new Error("Second param must be less than first.");
+    return a + Math.random()*(b-a);
   }
 }
